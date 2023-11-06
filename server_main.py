0e409fda59df4e801f7d72721382d203ad28e7e8
tinetantus <tine.tantus@gmail.com>
2023-11-06 15:03:02 +0100

    Adding function handle

--- server_main.py
+++ server_main.py
@@ -41,9 +41,30 @@ def insert_sqlite_db(conn, id, password, value=1):
         print(f'Error inserting table: f{e}')
 
 
-if __name__ == '__main__':
-
+def set_up():
     conn = connect_sqlite_db('database/server.db')
     set_up_sqlite_db(conn)
     insert_sqlite_db(conn, 1, "test_pass")
+
+
+# TODO: User provide id/pass
+#  -> check if id already exist -
+#       -> if yes check if pass is correct
+#           -> if yes allow in and return 'auth_token'
+#           -> if no reject with error "password incorrect"
+#       -> if no create new user with password -> return 'auth_token'
+def login(id, password):
+    pass
+
+
+def set_value(auth_token, value):
+    pass
+
+
+def logout(auth_token):
+    pass
+
+
+if __name__ == '__main__':
+    set_up()
 
