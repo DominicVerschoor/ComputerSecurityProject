9fa748c85e75804f589f3a1b657dc998b3404845
tinetantus <tine.tantus@gmail.com>
2023-12-08 12:17:24 +0100

    Fix it: input validation

--- client_main.py
+++ client_main.py
@@ -1,9 +1,19 @@
 import json
+from password_strength import PasswordPolicy
 from time import sleep
 
 import bcrypt
 import requests
 
+policy = PasswordPolicy.from_names(
+    length=10,
+    uppercase=1,
+    lowercase=1,
+    special=1,
+    nonletters=1,
+    entropybits=30,
+    strengthbits=0.6,
+)
 
 def start_client(client_data, client_salt):
     # Get server url
@@ -11,12 +21,13 @@ def start_client(client_data, client_salt):
     body = {}
 
     # Test connection
-    resp_test = requests.get(url, json=body, verify=False)
+    resp_test = requests.get(url, json=body, verify=True)
     print(resp_test.text)
 
     # Register user
-    local_hashed_password = bcrypt.hashpw(client_data['password'].encode('utf-8'), client_salt)
-    data = {'user_id': client_data['id'], 'password': str(local_hashed_password)}
+    if validate_password_input(client_data['password']):
+        local_hashed_password = bcrypt.hashpw(client_data['password'].encode('utf-8'), client_salt)
+        data = {'user_id': client_data['id'], 'password': str(local_hashed_password)}
 
     resp_register = requests.post(url + '/register', json=data, verify=False).json()
     print(resp_register)
@@ -47,6 +58,28 @@ def start_client(client_data, client_salt):
     # Check log file
     resp_check = requests.get(url + '/check', verify=False).json()
     print(f'Complete log file: {resp_check["log"]}')
+
+
+def validate_password_input(unvalidate_input, max_length=50, min_length=10):
+    # Type validation
+    if type(unvalidate_input) is not str:
+        return False
+
+    # Length validation
+    if (len(unvalidate_input) > max_length) & (len(unvalidate_input) <= min_length):
+        return False
+
+    # Character validation
+    if not str(unvalidate_input).isascii():
+        return False
+
+    if str(unvalidate_input).isalnum():
+        return False
+
+    if len(policy.test(str(unvalidate_input))) > 0:
+        return False
+
+    return True
 
 
 if __name__ == '__main__':
--- server_main.py
+++ server_main.py
@@ -21,12 +21,32 @@ class ServerSide:
         logging.basicConfig(filename='log/' + logfile, encoding='utf-8', level=logging.DEBUG)
         logging.info(f'Server setup time: {datetime.now()}')
 
+    def validate_id_input(self, unvalidate_input, max_length=24):
+        # Type validation
+        if type(unvalidate_input) is not str:
+            return False
+
+        # Length validation
+        if len(unvalidate_input) > max_length:
+            return False
+
+        # Character validation
+        if not str(unvalidate_input).isalnum():
+            return False
+
+        return True
+
     def register_user(self, new_id, new_pass):
-        new_id = str(new_id)
+        if self.validate_id_input(new_id):
+            new_id = str(new_id)
+        else:
+            return 300, 'Incorrect password or ID', None
+
         if not isinstance(new_pass, (bytes, bytearray)):
             new_pass = new_pass.encode('utf-8')
 
-        hash_new_pass = bcrypt.hashpw(new_pass, bcrypt.gensalt())
+        hash_new_pass = bcrypt.hashpw(new_pass,
+                                      bcrypt.gensalt())
 
         if new_id in self.user_db.keys():
 
@@ -39,7 +59,7 @@ class ServerSide:
                     f'USER_ID: {new_id} SESSION_ID {new_session_id} VALUE: {self.user_db[new_id]["value"]} MESSAGE: New session created. {datetime.now()}')
                 return 200, 'New session created.', new_session_id
 
-            return 300, 'Incorrect password', None
+            return 300, 'Incorrect password or ID', None
 
         new_session_id = str(uuid.uuid4())
         self.session_map[new_session_id] = new_id
@@ -49,7 +69,7 @@ class ServerSide:
             f'USER_ID: {new_id} SESSION_ID {new_session_id} VALUE: {self.user_db[new_id]["value"]} MESSAGE: New user registered. {datetime.now()}')
         return 200, 'New user registered.', new_session_id
 
-    def logout_user(self, session_id):
+    def logout_user(self, session_id):  # TODO: Input validation 1. length, 2. characters, 3. type
         if session_id in self.session_map.keys():
             user_id = self.session_map[session_id]
 
@@ -73,7 +93,7 @@ class ServerSide:
 
         return 300, 'Session do not exist.'
 
-    def increase_value(self, session_id, by):
+    def increase_value(self, session_id, by):  # TODO: Input validation 1. length, 2. characters, 3. type
         if session_id in self.session_map.keys():
             user_id = self.session_map[session_id]
 
@@ -93,7 +113,7 @@ class ServerSide:
         print('Session do not exist.')
         return 300, 'Session do not exist.'
 
-    def decrease_value(self, session_id, by):
+    def decrease_value(self, session_id, by):  # TODO: Input validation 1. length, 2. characters, 3. type
         if session_id in self.session_map.keys():
             user_id = self.session_map[session_id]
 
@@ -118,7 +138,6 @@ class ServerSide:
 
 
 if __name__ == '__main__':
-
     server = ServerSide()
 
     _, _, auth_1 = server.register_user(1, 'pass1')
